ae0bd6376121517a52cc2eb0986603b5ed02d752
pkdgood <sean4787@gmail.com>
2019-04-08 11:44:53 +0900

    Upload Project

---
+++ ColNameGenerator/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ColNameGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 15F);
+            this.label1.Location = new System.Drawing.Point(20, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "컬럼속성";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("굴림", 15F);
+            this.tbInput.ImeMode = System.Windows.Forms.ImeMode.Hangul;
+            this.tbInput.Location = new System.Drawing.Point(115, 117);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(285, 30);
+            this.tbInput.TabIndex = 1;
+            this.tbInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbInput_KeyDown);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Font = new System.Drawing.Font("굴림", 15F);
+            this.tbOutput.Location = new System.Drawing.Point(115, 243);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(285, 30);
+            this.tbOutput.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 15F);
+            this.label2.Location = new System.Drawing.Point(20, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "컬럼명";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 20F);
+            this.label3.Location = new System.Drawing.Point(245, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "↓";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 20F);
+            this.button1.Location = new System.Drawing.Point(432, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 79);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "변환";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 50F);
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(542, 67);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "컬럼 생성 도우미";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 15F);
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(28, 325);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(411, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "컬럼속성은 용어 단위로 띄어쓰기 해주세요 !";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 384);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ColNameGenerator/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColNameGenerator
+{
+    public partial class Form1 : Form
+    {
+        string currentPath;
+        public Form1()
+        {
+            InitializeComponent();
+            currentPath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input;
+            input = tbInput.Text;
+
+            tbOutput.Text = Generate(input);
+        }
+
+        public string Generate(string input)
+        {
+            string connStr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + currentPath + "\\DB_vocab.accdb";
+            string sql=null;
+            string output = null;
+            string[] words = input.Split(' ');
+
+            int i = 0;
+            bool fin = false;
+
+            using (OleDbConnection conn = new OleDbConnection(connStr))
+            {
+                conn.Open();
+                try
+                {
+                    while (true)
+                    {
+                        sql = "SELECT [Output] FROM [TableVocab] WHERE [Input]= '" + words[i] + "'";
+                        OleDbCommand cmd = new OleDbCommand(sql, conn);
+                        object scalarValue = cmd.ExecuteScalar();
+
+                        if(scalarValue==null)
+                        {
+                            MessageBox.Show("'" + words[i] + "' 은(는) 정의되지 않은 용어 입니다.\nDB에 추가해주세요.");
+                            break;
+                        }
+                        else
+                        {
+
+                            output += (string)scalarValue;
+                            output += '_';
+
+                            i++;
+                        }
+                    }
+                }
+                catch (IndexOutOfRangeException) { fin = true; }
+                finally
+                {
+                    if (output != null)
+                    {
+                        output = output.Remove(output.Length - 1, 1);
+                    }
+                }
+            }
+            if(fin == true)
+            {
+
+                return output;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void tbInput_KeyDown(object sender, KeyEventArgs e)
+        {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    this.button1_Click(sender, e);
+                }
+        }
+    }
+}
